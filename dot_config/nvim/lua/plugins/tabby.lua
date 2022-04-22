4d70116fe1cd7e6b78e529e4679071a9ebafc2a2
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2022-04-22 09:44:49 +0600

    Rewrite tabby

--- dot_config/nvim/lua/plugins/tabby.lua
+++ dot_config/nvim/lua/plugins/tabby.lua
@@ -1,94 +1,59 @@
 local util = require('tabby.util')
-local hl_tabline = util.extract_nvim_hl('TabLine')
-local hl_normal = util.extract_nvim_hl('Normal')
-local hl_tabline_sel = util.extract_nvim_hl('TabLineSel')
-local active_tab_hl = { fg = hl_normal.fg, bg = hl_normal.bg, style = 'bold' }
-local inactive_tab_hl = { fg = hl_tabline.fg, bg = hl_tabline.bg }
 
-local function fileIcon()
-	return '  ' .. require ('nvim-web-devicons').get_icon(vim.fn.expand('%:t'), vim.fn.expand('%:e'), {default = true}) .. '  '
-end
-
-----
-local function tab_label(tabid, active)
-  local icon = active and '' or ''
-  local number = vim.api.nvim_tabpage_get_number(tabid)
-  local name = util.get_tab_name(tabid)
-  return string.format(' %s %d: %s ', icon, number, name)
-end
-
-local function tab_label_no_fallback(tabid, active)
-  local icon = active and '' or ''
-  local fallback = function()
-    return ''
-  end
-  local number = vim.api.nvim_tabpage_get_number(tabid)
-  local name = util.get_tab_name(tabid, fallback)
-  if name == '' then
-    return string.format(' %s %d ', icon, number)
-  end
-  return string.format(' %s %d: %s ', icon, number, name)
-end
-
-local function win_label(winid, top)
-  local icon = top and '' or ''
-  local fname = require("tabby.filename").tail(winid)
-  local extension = vim.fn.fnamemodify(fname, ':e')
-  local fileIcon = require'nvim-web-devicons'.get_icon(filename, extension)
-  local buid = vim.api.nvim_win_get_buf(winid)
-  local is_modified = vim.api.nvim_buf_get_option(buid, 'modified')
-  local modifiedIcon = is_modified and '' or ''
-  return string.format(' %s  %s %s %s', icon, fileIcon, filename.unique(winid), modifiedIcon)
-end
+local colors = {
+  black        = '#3c3836',
+  white        = '#f9f5d7',
+  orange       = '#af3a03',
+  green        = '#427b58',
+  blue         = '#076678',
+  gray         = '#d5c4a1',
+  darkgray     = '#7c6f64',
+  lightgray    = '#ebdbb2',
+  inactivegray = '#a89984'
+} --gruvbox
+local hl_inactive = {bg = colors.darkgray, fg = colors.white}
+local hl_normal = {bg = colors.lightgray, fg = colors.darkgray}
+local hl_active = {bg = colors.white, fg = colors.darkgray}
+local hl_sep_active = { bg = colors.lightgray, fg = colors.white}
+local hl_sep_inactive = { bg = colors.lightgray, fg = colors.darkgray}
 
 local function buffer_render (bufid, is_current)
-	local buftype = vim.bo[bufid].buftype
 	local modified = vim.bo[bufid].modified
 	local modified_icon = modified and '' or ' '
 	local path = vim.fn.bufname(bufid);
 	local extension = vim.fn.fnamemodify(path, ":e")
 	local filename = vim.fn.fnamemodify(path, ":t")
 	local fileicon = require'nvim-web-devicons'.get_icon (filename, extension, { default = true})
-	local buf_name = ' ' .. fileicon .. ' ' .. filename ..' ' .. modified_icon .. ' '
-	local buf_color = is_current and active_tab_hl or inactive_tab_hl
-	return {
+	local buf_name = fileicon .. ' ' .. filename .. modified_icon
+	local buf_color = is_current and hl_active or hl_normal
+	return
+	{
 		type = 'text',
-		text = {
-			string.format(' %s ', buf_name) ,
-				hl = buf_color,
-			}
+		text =
+		{
+		string.format(' %s', buf_name) ,
+			hl = buf_color,
+		}
 	}
-end
-----
 
-local function gitBranch()
-	local cmd = io.popen('git branch --show-current 2> /dev/null')
-	local branch = cmd:read("*l") or cmd:read("*a")
-	cmd:close()
-
-	if branch ~= "" then
-		return '  ' .. branch .. ' '
-	else
-		return ''
-	end
 end
 
-local components = function()
+local components = function ()
 	local coms = {
 		{
 			type = 'text',
 			text = {
-				fileIcon,
-				hl = { fg = hl_tabline_sel.fg, bg = hl_tabline_sel.bg, style = 'bold' },
-			},
+				'  ',
+				hl = hl_inactive
+			}
 		},
 		{
 			type = 'text',
 			text = {
-				' ',
-				hl = 'TabLineFill'
+				' ',
+				hl = hl_sep_inactive
 			}
-		},
+		}
 	}
 	local tabs = vim.api.nvim_list_tabpages()
 	local current_tab = vim.api.nvim_get_current_tabpage()
@@ -99,8 +64,10 @@ local components = function()
 				tabid = tabid,
 				label = {
 					'  ' .. vim.api.nvim_tabpage_get_number(tabid) .. '  ',
-					hl = active_tab_hl
+					hl = hl_active
 				},
+				left_sep = { '', hl = hl_sep_active },
+        		right_sep = { '', hl = hl_sep_active },
 			})
 		end
 		if tabid ~= current_tab then
@@ -109,20 +76,12 @@ local components = function()
 				tabid = tabid,
 				label = {
 					'  ' .. vim.api.nvim_tabpage_get_number(tabid) .. '  ',
-					hl = inactive_tab_hl
+					hl = hl_inactive
 				},
 			})
 		end
 	end
-	table.insert(coms,
-		{
-			type = 'text',
-			text = {
-				' ',
-				hl = 'TabLineFill'
-			}
-		}
-	)
+
 	table.insert(coms, { type = 'spring' } )
 	local cur_bufid = vim.api.nvim_get_current_buf()
 	for _, bufid in ipairs(vim.api.nvim_list_bufs()) do
@@ -133,37 +92,21 @@ local components = function()
 				is_current = true
 			end
 			local buf_render = buffer_render (bufid,is_current)
-			-- local focus_win = vim.api.nvim_tabpage_get_win(tabid)
-			-- local filename = require("tabby.filename").tail(focus_win)
-			-- local extension =  vim.fn.fnamemodify(name, ':t')
-			-- local icon = require'nvim-web-devicons'.get_icon(filename, extension)
-			-- print (extension)
-				table.insert(coms, buf_render)
+			local buf_sep_color_front = is_current and {'', hl = hl_sep_active} or {' ', hl = hl_normal}
+			local buf_sep_color_end = is_current and {'', hl = hl_sep_active} or {' ', hl = hl_normal}
 			table.insert(coms,
-				{
-					type = 'text',
-					text = {
-						' ',
-						hl = 'TabLineFill'
-					}
-				}
-			)
+			{
+				type = 'text',
+				text = buf_sep_color_front
+			})
+			table.insert(coms, buf_render)
+			table.insert(coms,
+			{
+				type = 'text',
+				text = buf_sep_color_end
+			})
 		end
 	end
-	table.insert(coms,
-		{
-			type = 'text',
-			text = {
-				gitBranch,
-				hl = { fg = hl_tabline_sel.fg, bg = hl_tabline_sel.bg, style = 'bold' },
-			},
-		}
-	)
 	return coms
 end
-
-require('tabby').setup{
-	components = components,
-}
-
-
+require ('tabby').setup ({components = components})
