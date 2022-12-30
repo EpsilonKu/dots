1b65d728a3bfc40c0bfb4d9d5acbc37fa4a734fa
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2022-12-30 19:15:03 +0600

    Remove unnecessary

--- .dots/nvim/after/ftplugin/dart.lua
+++
@@ -1,9 +0,0 @@
-require 'key-menu'.set('n', '<Space>a',
-  { desc = 'Flutter', buffer = true })
-vim.keymap.set('n', '<Space>ar', '<cmd>FlutterRun<CR>', { desc = 'Flutter Run', buffer = true })
-vim.keymap.set('n', '<Space>ad', '<cmd>FlutterDevices<CR>', { desc = 'Flutter Devices', buffer = true })
-vim.keymap.set('n', '<Space>ae', '<cmd>FlutterEmulators<CR>', { desc = 'Flutter Emulators', buffer = true })
-vim.keymap.set('n', '<Space>aa', '<cmd>FlutterReload<CR>', { desc = 'Flutter Reload', buffer = true })
-vim.keymap.set('n', '<Space>as', '<cmd>FlutterRestart<CR>', { desc = 'Flutter Restart', buffer = true })
-vim.keymap.set('n', '<Space>ao', '<cmd>FlutterOutlineToggle<CR>', { desc = 'Flutter Outline', buffer = true })
-vim.keymap.set('n', '<Space>aq', '<cmd>FlutterQuit<CR>', { desc = 'Flutter Quit', buffer = true })
